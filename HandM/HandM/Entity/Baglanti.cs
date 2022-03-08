39c5fbd5559f2ea94c934f479a0a31f8bf2545fa
Erkan Bostan <profesyonel59@gmail.com>
2022-03-08 04:52:15 +0300

    Kurumsal Web Sitesi
    Form İşlemlerinin Yanında Asp.Net Kurumsal Web Projesi (Reklam İçerir!)

---
+++ HandM/HandM/Entity/Baglanti.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace HandM.Entity
+{
+    public  static class Baglanti
+    {
+        public static HandMDBEntities db = new HandMDBEntities();
+        public static SqlConnection bgl = new SqlConnection("data source=GEOPC\\SQLEXPRESS;initial catalog=HandMDB;integrated security=True;MultipleActiveResultSets=True;");
+    }
+}
